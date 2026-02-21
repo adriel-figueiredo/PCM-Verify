b2a968718e04aad9c5f4fd510f5eead8b00a1418
adriel-figueiredo <adriel.dev.br@gmail.com>
2026-02-21 15:47:34 -0300

    merge fix

--- src/env.ts
+++ src/env.ts
@@ -7,11 +7,7 @@ export const env = await validateEnv(z.looseObject({
     MAIN_SERVER: z.string().min(1),
     MAIN_VERIFY: z.string().min(1),
     DATABASE_KEY: z.string().min(1),
-<<<<<<< HEAD
     DATABASE_URL: z.url(),
-=======
-    DATABASE_URL: z.url().optional(),
->>>>>>> 4c79161ac2eb8af24dc5450744c05e7dd3ddf35a
     WEBHOOK_LOGS_URL: z.url().optional(),
     SERVER_PORT: z.coerce.number().min(1).optional()
 }));
